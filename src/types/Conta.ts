092fd938929b6148626dfd8e09410f336f7bfee4
Edilson Belo <ebelo@MacBook-Air-de-Edilson.local>
2024-07-13 00:51:07 -0300

    update

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,49 +1,54 @@
-import { Transacao } from "./Transacao.js";
-import { TipoTransacao } from "./TipoTransacao.js";
-import { GrupoTransacao } from "./GrupoTransacao.js";
+import { Armazenador } from "../utils/Armazenador";
+import { GrupoTransacao } from "./GrupoTransacao";
+import { TipoTransacao } from "./TipoTransacao";
+import { Transacao } from "./Transacao";
 
-let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
-const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
-    if (key === "data") {
-        return new Date(value);
+export class Conta {
+
+    protected nome: string
+    protected saldo: number = Armazenador.obter("saldo") || 0; 
+    private transacoes: Transacao[] = Armazenador.obter(("transacoes"), (key: string, value: string) => {
+        
+        if (key === "data") {
+            return new Date(value);
+        }
+    
+        return value;
+    }) || [];
+
+
+    private debitar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser debitado deve ser maior que zero!");
+        }
+        if (valor > this.saldo) {
+            throw new Error("Saldo insuficiente!");
+        }
+    
+        this.saldo -= valor;
+        Armazenador.salvar("saldo", this.saldo.toString());
     }
 
-    return value;
-}) || [];
-
-function debitar(valor: number): void {
-    if (valor <= 0) {
-        throw new Error("O valor a ser debitado deve ser maior que zero!");
-    }
-    if (valor > saldo) {
-        throw new Error("Saldo insuficiente!");
+    private depositar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser depositado deve ser maior que zero!");
+        }
+    
+        this.saldo += valor;
+        Armazenador.salvar("saldo", this.saldo.toString());
     }
 
-    saldo -= valor;
-    localStorage.setItem("saldo", saldo.toString());
-}
-
-function depositar(valor: number): void {
-    if (valor <= 0) {
-        throw new Error("O valor a ser depositado deve ser maior que zero!");
+    public getSaldo() {
+        return this.saldo;
     }
 
-    saldo += valor;
-    localStorage.setItem("saldo", saldo.toString());
-}
-
-const Conta = {
-    getSaldo() {
-        return saldo;
-    },
-
-    getDataAcesso(): Date {
+    public getDataAcesso(): Date {
         return new Date();
-    },
+    }
 
-    getGruposTransacoes(): GrupoTransacao[] {
+    private getGruposTransacoes(): GrupoTransacao[] {
         const gruposTransacoes: GrupoTransacao[] = [];
-        const listaTransacoes: Transacao[] = structuredClone(transacoes);
+        const listaTransacoes: Transacao[] = structuredClone(this.transacoes);
         const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
         let labelAtualGrupoTransacao: string = "";
 
@@ -60,24 +65,25 @@ const Conta = {
         }
 
         return gruposTransacoes;
-    },
+    }
 
-    registrarTransacao(novaTransacao: Transacao): void {
+    public registrarTransacao(novaTransacao: Transacao): void {
         if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-            depositar(novaTransacao.valor);
+            this.depositar(novaTransacao.valor);
         } 
         else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
-            debitar(novaTransacao.valor);
+            this.debitar(novaTransacao.valor);
             novaTransacao.valor *= -1;
         } 
         else {
             throw new Error("Tipo de Transação é inválido!");
         }
 
-        transacoes.push(novaTransacao);
+        this.transacoes.push(novaTransacao);
         console.log(this.getGruposTransacoes());
-        localStorage.setItem("transacoes", JSON.stringify(transacoes));
+        Armazenador.salvar("transacoes", JSON.stringify(this.transacoes));
     }
+
 }
 
 export default Conta;
